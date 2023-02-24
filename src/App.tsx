26fbdae9529d1000f7236b30390673e12198ed33
Jérémy <jeremy.vaneste@free.fr>
2023-02-24 10:48:33 +0100

    typing App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <p>REACT APP</p>
     </div>
   );
-}
+};
 
 export default App;
