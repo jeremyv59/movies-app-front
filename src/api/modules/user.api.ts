39d2562901175d32be7f1e368da83623d1432e97
Jérémy <jeremy.vaneste@free.fr>
2023-02-27 12:34:12 +0100

    create signin function to userApi

--- src/api/modules/user.api.ts
+++ src/api/modules/user.api.ts
@@ -10,6 +10,11 @@ interface IUserEndpoints {
   addFavorite: string;
 }
 
+interface SigninParams {
+  username: string;
+  password: string;
+}
+
 const userEndpoints: IUserEndpoints = {
   signin: "user/signin",
   signup: "user/signup",
@@ -17,4 +22,24 @@ const userEndpoints: IUserEndpoints = {
   passwordUpdate: "user/update-password",
   getFavorites: "user/favorites",
   addFavorite: "user/favorites",
+};
+
+const userApi = {
+  signin: async ({ username, password }: SigninParams) => {
+    try {
+      const response: Response = await publicClient.post(userEndpoints.signin, {
+        username,
+        password,
+      });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  signup: async ({}) => {
+    try {
+    } catch (err) {
+      return { err };
+    }
+  },
 };
